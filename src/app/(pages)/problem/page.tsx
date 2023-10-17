dc2683cf0264a7c4a67e97343c0a6135738b88d2
五月的夏天 <285258675@qq.com>
2023-10-17 21:44:18 +0800

    feat: 完成解答错误布局

--- src/app/(pages)/problem/page.tsx
+++ src/app/(pages)/problem/page.tsx
@@ -1,15 +1,16 @@
 'use client'
 
-import Content from '@/app/components/problemDetail/Content'
+import Content, { MenuItem } from '@/app/components/problemDetail/Content'
 import Editor from '@/app/components/problemDetail/Editor'
 import Header from '@/app/components/problemDetail/Header'
-import { Button } from 'antd'
 import axios from 'axios'
-import { useSearchParams } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 const Problem = () => {
   const params = useSearchParams()
+  const pathname = usePathname()
+  const router = useRouter()
   const slugTitle = useMemo(() => {
     return params.get('slugTitle')
   }, [params])
@@ -24,6 +25,59 @@ const Problem = () => {
   }, [params])
   const editorRef = useRef<any>()
   const [detail, setDetail] = useState()
+  const [items, setItems] = useState<MenuItem[]>([
+    {
+      label: '题目描述',
+      key: 'problem',
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 14 14"
+          width="1em"
+          height="1em"
+          fill="currentColor"
+          className="h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-[#007aff]">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.9 4.2h4.2M4.9 6.3h4.2M4.9 8.4H7m-3.15-7h6.3a1.4 1.4 0 011.4 1.4v8.4a1.4 1.4 0 01-1.4 1.4h-6.3a1.4 1.4 0 01-1.4-1.4V2.8a1.4 1.4 0 011.4-1.4z"></path>
+        </svg>
+      ),
+    },
+    {
+      label: '题解',
+      key: 'solution',
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 14 14"
+          width="1em"
+          height="1em"
+          fill="currentColor"
+          className="h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-[#007aff]">
+          <path d="M4.367 1.633h5.047m-4.17 4.093V1.692c0-.033.027-.059.059-.059h3.251c.032 0 .058.026.058.059v4.457c0 .012.004.023.01.033l2.754 3.977c.646.933-.022 2.208-1.157 2.208H3.73a1.407 1.407 0 01-1.17-2.19l2.675-3.995c.006-.01.01-.021.01-.033v-.423zm0 0H6.67M5.244 3.65H6.67"></path>
+        </svg>
+      ),
+    },
+    {
+      label: '提交记录',
+      key: 'submission',
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 14 14"
+          width="1em"
+          height="1em"
+          fill="currentColor"
+          className="h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-[#007aff]">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M11.97 7a5.6 5.6 0 01-5.602 5.6 5.6 5.6 0 114.852-8.4m-.787 3.414l1.4-1.4 1.4 1.4m-4.766.913l-2.1-.7V4.9"></path>
+        </svg>
+      ),
+    },
+  ])
 
   const getProblemDetail = useCallback(() => {
     axios
@@ -44,7 +98,32 @@ const Problem = () => {
         slugTitle,
       })
       .then(res => {
-        console.log(res.data)
+        const data = res.data
+        if (data.code === 200 && data.data) {
+          if (data.data.status_msg === 'Wrong Answer') {
+            items.push({
+              label: '解答错误',
+              key: 'answer',
+              icon: (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 14 14"
+                  width="1em"
+                  height="1em"
+                  fill="currentColor"
+                  className="h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-[#007aff]">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.97 7a5.6 5.6 0 01-5.602 5.6 5.6 5.6 0 114.852-8.4m-.787 3.414l1.4-1.4 1.4 1.4m-4.766.913l-2.1-.7V4.9"></path>
+                </svg>
+              ),
+            })
+            setItems([...items])
+            router.push(`${pathname}?title=${titleCn}&slugTitle=${slugTitle}&type=answer`)
+          }
+        }
+        console.log(res.data.data)
       })
   }
 
@@ -53,13 +132,19 @@ const Problem = () => {
   }, [getProblemDetail])
 
   return (
-    <div className='h-[100vh] bg-[#f0f0f0] overflow-hidden'>
+    <div className="h-[100vh] bg-[#f0f0f0] overflow-hidden">
       <Header submit={submit}></Header>
-      <div className='flex h-full' id='mainContent'>
-        <div className='flex-1 mr-4'>
-          <Content detail={detail!} title={titleCn!} slugTitle={slugTitle!}></Content>
+      <div
+        className="flex h-full"
+        id="mainContent">
+        <div className="flex-1 mr-4">
+          <Content
+            detail={detail!}
+            title={titleCn!}
+            slugTitle={slugTitle!}
+            items={items}></Content>
         </div>
-        <div className='flex-1'>
+        <div className="flex-1">
           <Editor ref={editorRef}></Editor>
         </div>
       </div>
--- src/app/components/problemDetail/Content.tsx
+++ src/app/components/problemDetail/Content.tsx
@@ -7,21 +7,22 @@ import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { useMemo } from 'react'
 import Solution from './Solution'
 import Submission from './Submission'
+import Answer from './Answer'
 
 interface Props {
   detail: ProblemDetail
   title: string
   slugTitle: string
+  items: MenuItem[]
 }
 
-interface MenuItem {
+export interface MenuItem {
   key: string
   label: string
   icon: React.ReactNode
 }
-
 const Content = (props: Props) => {
-  const { detail, title, slugTitle } = props
+  const { detail, title, slugTitle, items } = props
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const router = useRouter()
@@ -29,87 +30,30 @@ const Content = (props: Props) => {
   const type = useMemo(() => {
     return searchParams.get('type') || 'problem'
   }, [searchParams])
-  const items: MenuItem[] = [
-    {
-      label: '题目描述',
-      key: 'problem',
-      icon: (
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 14 14'
-          width='1em'
-          height='1em'
-          fill='currentColor'
-          className='h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-blue-60 dark:text-blue-60'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M4.9 4.2h4.2M4.9 6.3h4.2M4.9 8.4H7m-3.15-7h6.3a1.4 1.4 0 011.4 1.4v8.4a1.4 1.4 0 01-1.4 1.4h-6.3a1.4 1.4 0 01-1.4-1.4V2.8a1.4 1.4 0 011.4-1.4z'
-          ></path>
-        </svg>
-      ),
-    },
-    {
-      label: '题解',
-      key: 'solution',
-      icon: (
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 14 14'
-          width='1em'
-          height='1em'
-          fill='currentColor'
-          className='h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-blue-60 dark:text-blue-60'
-        >
-          <path d='M4.367 1.633h5.047m-4.17 4.093V1.692c0-.033.027-.059.059-.059h3.251c.032 0 .058.026.058.059v4.457c0 .012.004.023.01.033l2.754 3.977c.646.933-.022 2.208-1.157 2.208H3.73a1.407 1.407 0 01-1.17-2.19l2.675-3.995c.006-.01.01-.021.01-.033v-.423zm0 0H6.67M5.244 3.65H6.67'></path>
-        </svg>
-      ),
-    },
-    {
-      label: '提交记录',
-      key: 'submission',
-      icon: (
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 14 14'
-          width='1em'
-          height='1em'
-          fill='currentColor'
-          className='h-3.5 w-3.5 flex-none fill-none stroke-current stroke-1.5 text-blue-60 dark:text-blue-60'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M11.97 7a5.6 5.6 0 01-5.602 5.6 5.6 5.6 0 114.852-8.4m-.787 3.414l1.4-1.4 1.4 1.4m-4.766.913l-2.1-.7V4.9'
-          ></path>
-        </svg>
-      ),
-    },
-  ]
 
   return (
-    <div className='m-[10px] pl-5 py-2 bg-white h-full w-full'>
-      <div style={{ borderBottom: '1px solid #eee' }} className='fixed '>
-        <div className='flex items-center px-4 h-9'>
-          <div className='flex items-center h-full gap-2'>
+    <div className="m-[10px] pl-5 py-2 bg-white h-full w-full">
+      <div className="fixed ">
+        <div className="flex items-center px-4 h-9">
+          <div className="flex items-center h-full gap-2">
             {items.map((item, index) => {
               return (
                 <div
                   key={item.key}
-                  className='relative flex items-center cursor-pointer overflow-hidden text-xs capitalize  hover:bg-[#0000000a]'
+                  className="relative flex items-center cursor-pointer overflow-hidden text-xs capitalize  hover:bg-[#0000000a]"
                   style={{
                     background: type === item.key ? '#0000' : '',
                     color: type === item.key ? '#1a1a1a' : '#0000008c',
                     fontWeight: type === item.key ? 500 : 400,
                   }}
                   onClick={() => {
-                    router.push(`${pathname}?title=${title}&slugTitle=${slugTitle}&type=${item.key}`)
-                  }}
-                >
-                  <div className='h-full flex items-center text-[#007aff]'>{item.icon}</div>
-                  <div className='h-full flex items-center ml-1'>{item.label}</div>
-                  {index !== items.length - 1 ? <Divider type='vertical'></Divider> : null}
+                    router.push(
+                      `${pathname}?title=${title}&slugTitle=${slugTitle}&type=${item.key}`
+                    )
+                  }}>
+                  <div className="h-full flex items-center text-[#007aff]">{item.icon}</div>
+                  <div className="h-full flex items-center ml-1">{item.label}</div>
+                  {index !== items.length - 1 ? <Divider type="vertical"></Divider> : null}
                 </div>
               )
             })}
@@ -117,18 +61,26 @@ const Content = (props: Props) => {
         </div>
       </div>
       {type === 'problem' ? (
-        <div className='mt-10 h-full pb-[100px]'>
-          <Description detail={detail} title={title} slugTitle={slugTitle}></Description>
+        <div className="mt-10 h-full pb-[100px]">
+          <Description
+            detail={detail}
+            title={title}
+            slugTitle={slugTitle}></Description>
         </div>
       ) : null}
       {type === 'solution' ? (
-        <div className='mt-10 h-full'>
+        <div className="mt-10 h-full">
           <Solution></Solution>
         </div>
       ) : null}
       {type === 'submission' ? (
-        <div className='mt-10 h-full'>
+        <div className="mt-10 h-full">
           <Submission></Submission>
+        </div>
+      ) : null}
+      {type === 'answer' ? (
+        <div className="mt-10 h-full">
+          <Answer></Answer>
         </div>
       ) : null}
     </div>
