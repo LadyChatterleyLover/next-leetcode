6bc7498be4b0cea5afe21a0b1e075c6578798834
骆沛 <luopei@11.com>
2023-10-20 09:33:21 +0800

    refactor: 调整首页布局

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,9 +3,9 @@ import Categories from '../components/home/Categories'
 
 const Home = () => {
   return (
-    <>
+    <div className='mx-auto w-full grow p-4 md:mt-0 md:max-w-[888px] md:p-6 lg:max-w-[1100px] mt-[50px] bg-white'>
       <Categories></Categories>
-    </>
+    </div>
   )
 }
 
